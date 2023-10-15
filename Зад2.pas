017fd51d745a275a7395418a16ef1c3c743399b0
1loYed1 <144238426+1loYed1@users.noreply.github.com>
2023-10-15 21:33:40 +0300

    Add files via upload

---
+++ Зад2.pas
@@ -0,0 +1,23 @@
+﻿Program lab1;
+var x, y: real;
+begin
+  Writeln('Введите x: ');
+  readln(x);
+    if x < -7 then begin
+      writeln ('Тогда при x = ' ,  x:2:2,  ' нет решений');
+    end;
+      if (x>=-7) and (x<-4) then begin
+        writeln ('Тогда при x = ' ,  x:2:2,  ' нет решений');
+      end;
+      if (x>=-4) and (x<6) then begin
+        y:= tan(x)/power(x,1/3);
+      if y = y then
+        writeln('при x = ',x:2:2 , ' y = ',y:2:2)
+      else
+        writeln('Тогда при x = ' ,  x:2:2,  ' нет решений')
+      end;
+      if (x>=6) then begin
+        y:=tan(x)/power(x,2);
+        writeln('при x = ',x:2:2 , ' y = ',y:2:2)
+      end;
+end.
